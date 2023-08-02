1f429cc5b52fc3fa50c28e3a8b2cbc2d740f5b96
Aditya <rajaditya3759@gmail.com>
2023-08-02 09:09:56 +0530

    Test: Transfer functio is tested

--- test/uint/AdityaToken.t.sol
+++ test/uint/AdityaToken.t.sol
@@ -8,6 +8,14 @@ import {AdityaToken} from "../../src/AdityaToken.sol";
 
 contract AdityaTokenTest is Test {
     AdityaToken adityaToken;
+    address USER1 = makeAddr("user1");
+    address USER2 = makeAddr("user2");
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint indexed amount
+    );
 
     function setUp() external {
         DeployAdityaToken deployAdityaToken = new DeployAdityaToken();
@@ -34,5 +42,65 @@ contract AdityaTokenTest is Test {
         address owner = adityaToken.i_owner();
         uint initialOwnerBalance = adityaToken.balanceOf(owner);
         assert(initialOwnerBalance == totalSupply);
+    }
+
+    function testTransferFailIfOwnerHasNotZeroBalance() public {
+        uint initialBalanceOfUser1 = adityaToken.balanceOf(USER1);
+        uint initialBalanceOfUser2 = adityaToken.balanceOf(USER2);
+        assert(initialBalanceOfUser1 == 0);
+        assert(initialBalanceOfUser2 == 0);
+
+        vm.prank(USER1);
+        vm.expectRevert();
+        adityaToken.transfer(USER2, 20);
+
+        uint finalBalanceOfUser2 = adityaToken.balanceOf(USER2);
+        assert(finalBalanceOfUser2 == initialBalanceOfUser2);
+    }
+
+    function testTransferFailIfOwnerHasNotEnoughBalance() public {
+        vm.prank(msg.sender);
+        adityaToken.transfer(USER1, 200);
+
+        uint initialBalanceOfUser1 = adityaToken.balanceOf(USER1);
+        uint initialBalanceOfUser2 = adityaToken.balanceOf(USER2);
+        assert(initialBalanceOfUser1 == 200);
+        assert(initialBalanceOfUser2 == 0);
+
+        vm.prank(USER1);
+        vm.expectRevert();
+        adityaToken.transfer(USER2, 300);
+
+        uint finalBalanceOfUser2 = adityaToken.balanceOf(USER2);
+        uint finalBalanceOfUser1 = adityaToken.balanceOf(USER1);
+        assert(finalBalanceOfUser2 == initialBalanceOfUser2);
+        assert(finalBalanceOfUser1 == initialBalanceOfUser1);
+    }
+
+    function testTransferPassAndUpdateMappings() public {
+        uint initialBalanceOfUser1 = adityaToken.balanceOf(USER1);
+        uint initialBalanceOfOwner = adityaToken.balanceOf(msg.sender);
+        uint totalSupply = adityaToken.i_totalSupply();
+
+        assert(initialBalanceOfOwner == totalSupply);
+        assert(initialBalanceOfUser1 == 0);
+
+        vm.prank(msg.sender);
+        adityaToken.transfer(USER1, 200);
+
+        uint finalBalanceOfUser1 = adityaToken.balanceOf(USER1);
+        uint finalBalanceOfOwner = adityaToken.balanceOf(msg.sender);
+
+        assert(finalBalanceOfUser1 == 200);
+        assert(
+            finalBalanceOfOwner == initialBalanceOfOwner - finalBalanceOfUser1
+        );
+    }
+
+    function testTransferShouldEmitWhenItsSuccess() public {
+        vm.prank(msg.sender);
+        vm.expectEmit(address(adityaToken));
+        emit Transfer(msg.sender, USER1, 200);
+        adityaToken.transfer(USER1, 200);
     }
 }

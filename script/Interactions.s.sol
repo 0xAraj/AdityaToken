30a2ca3f9297a45ceb9f312136a768f375d48ad8
Aditya <rajaditya3759@gmail.com>
2023-08-08 12:16:40 +0530

    Test: All integration test is done

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -25,3 +25,33 @@ contract Transfer is Script {
         adityaToken.transfer(recipient, amount);
     }
 }
+
+contract ApproveAndAllowance is Script {
+    function approve(address spender, uint amount) public returns (uint256) {
+        AdityaToken adityaToken = AdityaToken(
+            0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F
+        );
+        vm.prank(0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96);
+        adityaToken.approve(spender, amount);
+
+        uint256 approvedAmount = adityaToken.allowance(
+            0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96,
+            spender
+        );
+        return approvedAmount;
+    }
+}
+
+contract TransferFrom is Script {
+    function transferFrom(
+        address owner,
+        address spender,
+        uint256 amount
+    ) public {
+        AdityaToken adityaToken = AdityaToken(
+            0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F
+        );
+
+        adityaToken.transferFrom(owner, spender, amount);
+    }
+}
--- test/integration/AdityaTokenIntegrationTest.t.sol
+++ test/integration/AdityaTokenIntegrationTest.t.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {DeployAdityaToken} from "../../script/DeployAdityaToken.s.sol";
 import {AdityaToken} from "../../src/AdityaToken.sol";
-import {BalanceOf, Transfer} from "../../script/Interactions.s.sol";
+import {BalanceOf, Transfer, ApproveAndAllowance, TransferFrom} from "../../script/Interactions.s.sol";
 
 contract AdityaTokenIntegrationTest is Test {
     AdityaToken adityaToken;
@@ -37,5 +37,30 @@ contract AdityaTokenIntegrationTest is Test {
             0x83fD3360Bf77a6E0E27e3F8a31759Ff7d37CB534
         );
         assert(value == transferAmount);
+
+        // Approve and Allowance funciton is testing
+
+        ApproveAndAllowance approveAndAllownace = new ApproveAndAllowance();
+        uint256 approvedAmount = approveAndAllownace.approve(
+            0x83fD3360Bf77a6E0E27e3F8a31759Ff7d37CB534,
+            300
+        );
+        assert(approvedAmount == 300);
+
+        // TransferFrom function is testing
+
+        TransferFrom transferFrom = new TransferFrom();
+        transferFrom.transferFrom(
+            0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96,
+            0x83fD3360Bf77a6E0E27e3F8a31759Ff7d37CB534,
+            300
+        );
+
+        uint balanceOfSpender = balanceOf.balanceOf(
+            0x83fD3360Bf77a6E0E27e3F8a31759Ff7d37CB534
+        );
+
+        console.log(balanceOfSpender);
+        assert(balanceOfSpender == 1300); // Above we transfered 1000 and here 300, so total 1000+300 = 1300
     }
 }

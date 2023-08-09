29b1a7de6256526023406ad115fcfc4d4acf64ef
Aditya <rajaditya3759@gmail.com>
2023-08-09 10:43:29 +0530

    Refactor: Magic number are stored in variables

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,10 +6,11 @@ import {Script} from "forge-std/Script.sol";
 import {AdityaToken} from "../src/AdityaToken.sol";
 
 contract BalanceOf is Script {
+    address sepoliaSmartContractAddress =
+        0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F;
+
     function balanceOf(address owner) public view returns (uint256) {
-        AdityaToken adityaToken = AdityaToken(
-            0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F //sepolia address where smart contract is deployed
-        );
+        AdityaToken adityaToken = AdityaToken(sepoliaSmartContractAddress);
 
         uint256 amount = adityaToken.balanceOf(owner);
         return amount;
@@ -17,25 +18,31 @@ contract BalanceOf is Script {
 }
 
 contract Transfer is Script {
+    address sepoliaSmartContractAddress =
+        0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F;
+
+    address deployerAddress = 0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96;
+
     function transfer(address recipient, uint amount) public {
-        AdityaToken adityaToken = AdityaToken(
-            0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F
-        );
-        vm.prank(0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96); //address which deployed the contract
+        AdityaToken adityaToken = AdityaToken(sepoliaSmartContractAddress);
+        vm.prank(deployerAddress); //address which deployed the contract
         adityaToken.transfer(recipient, amount);
     }
 }
 
 contract ApproveAndAllowance is Script {
+    address sepoliaSmartContractAddress =
+        0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F;
+
+    address deployerAddress = 0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96;
+
     function approve(address spender, uint amount) public returns (uint256) {
-        AdityaToken adityaToken = AdityaToken(
-            0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F
-        );
-        vm.prank(0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96);
+        AdityaToken adityaToken = AdityaToken(sepoliaSmartContractAddress);
+        vm.prank(deployerAddress);
         adityaToken.approve(spender, amount);
 
         uint256 approvedAmount = adityaToken.allowance(
-            0x1AA18da866Bc70A78111b4C3F65883Dc8D8A0D96,
+            deployerAddress,
             spender
         );
         return approvedAmount;
@@ -43,14 +50,15 @@ contract ApproveAndAllowance is Script {
 }
 
 contract TransferFrom is Script {
+    address sepoliaSmartContractAddress =
+        0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F;
+
     function transferFrom(
         address owner,
         address spender,
         uint256 amount
     ) public {
-        AdityaToken adityaToken = AdityaToken(
-            0x9B31Fdc5f7aDd0Ad73EE941c6E8c1B122696A35F
-        );
+        AdityaToken adityaToken = AdityaToken(sepoliaSmartContractAddress);
 
         adityaToken.transferFrom(owner, spender, amount);
     }
